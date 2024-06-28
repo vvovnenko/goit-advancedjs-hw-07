34870db3f8c69da9434dc7b98d78bcc78f72f2dd
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-06-28 12:21:08 +0300

    add hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random()
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+abstract class House {
+  private door: boolean;
+  private tenants: Person[];
+
+  constructor(private key: Key) {
+    this.door = false;
+    this.tenants = [];
+  }
+
+  public comeIn(person: Person): void {
+    if (!this.door) {
+      throw new Error("The door is closed.");
+    }
+    this.tenants.push(person);
+  }
+
+  protected doOpenDoor(): void {
+    this.door = true;
+  }
+
+  protected validateKey(key: Key): boolean {
+    return this.key.getSignature() === key.getSignature();
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (!this.validateKey(key)) {
+      throw new Error("The key is not valid.");
+    }
+
+    this.doOpenDoor();
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
